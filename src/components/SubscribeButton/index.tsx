ed8e4f01fbd720a06ef68a1437c042565864ccac
paulobeckman <beckmam.paulo@hotmail.com>
2022-01-12 11:10:18 -0300

    feat: start test component SignInButton and SubscribeButton

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -4,11 +4,7 @@ import { api } from '../../services/api';
 import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss'
 
-interface SubscriberButtonProps {
-  priceId: string;
-}
-
-export function SubscribeButton({ priceId }: SubscriberButtonProps) {
+export function SubscribeButton() {
   const [session] =useSession();
   const router = useRouter();
 
